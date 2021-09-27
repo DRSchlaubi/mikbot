d5b543a4c1c4fb76167aa95728180adbf9f23452
Michael Rittmeister <michael@rittmeister.in>
2021-09-27 17:47:25 +0200

    Make now playing command also show other songs by queue postions

--- src/main/kotlin/dev/schlaubi/musicbot/module/music/commands/NowPlayingCommand.kt
+++ src/main/kotlin/dev/schlaubi/musicbot/module/music/commands/NowPlayingCommand.kt
@@ -1,5 +1,7 @@
 package dev.schlaubi.musicbot.module.music.commands
 
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalInt
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.interactions.respond
 import dev.kord.rest.builder.message.create.embed
@@ -7,7 +9,11 @@ import dev.schlaubi.musicbot.module.music.MusicModule
 import dev.schlaubi.musicbot.module.music.checks.anyMusicPlaying
 import dev.schlaubi.musicbot.utils.addSong
 
-suspend fun MusicModule.nowPlayingCommand() = publicSlashCommand {
+class NowPlayingArguments : Arguments() {
+    val index by optionalInt("position", "The number of the song in the queue to display the info for")
+}
+
+suspend fun MusicModule.nowPlayingCommand() = publicSlashCommand(::NowPlayingArguments) {
     name = "now-playing"
     description = "Displays information about the currently playing track"
 
@@ -16,7 +22,13 @@ suspend fun MusicModule.nowPlayingCommand() = publicSlashCommand {
     }
 
     action {
-        val playingTrack = player.playingTrack ?: return@action
+        val index = arguments.index
+        val playingTrack = if (index != null) {
+            musicPlayer.queuedTracks.getOrNull(index) ?: run {
+                respond { translate("commands.now_playing.invalid_index") }
+                return@action
+            }
+        } else player.playingTrack ?: return@action
 
         respond {
             embed {

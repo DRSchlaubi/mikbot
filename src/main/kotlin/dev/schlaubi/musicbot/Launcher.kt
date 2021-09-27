bc0b01adc64caaa554e3cfbb52e6235ab1a7aec3
Michael Rittmeister <michael@rittmeister.in>
2021-09-27 17:47:47 +0200

    No longer register for Spotify API

--- src/main/kotlin/dev/schlaubi/musicbot/Launcher.kt
+++ src/main/kotlin/dev/schlaubi/musicbot/Launcher.kt
@@ -3,7 +3,6 @@ package dev.schlaubi.musicbot
 import ch.qos.logback.classic.Logger
 import dev.schlaubi.musicbot.config.Config
 import dev.schlaubi.musicbot.core.MusicBot
-import dev.schlaubi.musicbot.module.music.player.queue.initializeSpotifyApi
 import io.sentry.Sentry
 import io.sentry.SentryOptions
 import org.slf4j.LoggerFactory
@@ -11,7 +10,6 @@ import org.slf4j.LoggerFactory
 suspend fun main() {
     initializeLogging()
     initializeSentry()
-    initializeSpotifyApi()
     MusicBot().start()
 }
 

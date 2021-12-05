eb9b0da2ee292c36408cd1522a7dd69f95cd4429
StckOverflw <stckoverflw@gmail.com>
2021-12-05 21:20:01 +0100

    Message server owner when something went wrong assigning the role on join

--- utils/role-selector/src/main/kotlin/dev/schlaubi/mikbot/utils/roleselector/listener/GuildMemberAddListener.kt
+++ utils/role-selector/src/main/kotlin/dev/schlaubi/mikbot/utils/roleselector/listener/GuildMemberAddListener.kt
@@ -1,13 +1,23 @@
 package dev.schlaubi.mikbot.utils.roleselector.listener
 
 import com.kotlindiscord.kord.extensions.extensions.event
+import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.event.guild.MemberJoinEvent
+import dev.kord.rest.request.RestRequestException
 import dev.schlaubi.mikbot.utils.roleselector.RoleSelectorDatabase
 import dev.schlaubi.mikbot.utils.roleselector.RoleSelectorModule
 
 suspend fun RoleSelectorModule.guildMemeberAddListener() = event<MemberJoinEvent> {
     action {
         val role = RoleSelectorDatabase.autoRoleCollection.findOneById(event.guildId) ?: return@action
-        event.member.addRole(role.roleId, "Auto Role")
+        try {
+            event.member.addRole(role.roleId, "Auto Role")
+        } catch (ex: RestRequestException) {
+            event.guild.asGuild().owner.getDmChannel().createMessage {
+                content = "Something went wrong while trying to give the set Auto Role to the new joining Member\n" +
+                        "Error: ${ex.error?.message}"
+            }
+        }
+
     }
 }

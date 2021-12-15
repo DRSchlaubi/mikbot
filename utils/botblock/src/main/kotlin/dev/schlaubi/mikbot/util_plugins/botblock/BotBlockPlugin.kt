d610d7027be1c77f085a3282f8700db86731c9f5
Michael Rittmeister <michael@rittmeister.in>
2021-12-15 04:24:20 +0100

    Add botblock plugin

---
+++ utils/botblock/src/main/kotlin/dev/schlaubi/mikbot/util_plugins/botblock/BotBlockPlugin.kt
@@ -0,0 +1,47 @@
+package dev.schlaubi.mikbot.util_plugins.botblock
+
+import com.kotlindiscord.kord.extensions.builders.ExtensibleBotBuilder
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.event
+import dev.kord.core.event.gateway.ReadyEvent
+import dev.kord.rest.request.KtorRequestException
+import dev.schlaubi.mikbot.plugin.api.Plugin
+import dev.schlaubi.mikbot.plugin.api.PluginMain
+import dev.schlaubi.mikbot.plugin.api.PluginWrapper
+import kotlinx.coroutines.*
+import mu.KotlinLogging
+import kotlin.time.Duration
+
+private val LOG = KotlinLogging.logger { }
+
+@PluginMain
+class BotBlockPlugin(wrapper: PluginWrapper) : Plugin(wrapper) {
+    override fun ExtensibleBotBuilder.ExtensionsBuilder.addExtensions() {
+        add(::BotBlockExtension)
+    }
+}
+
+class BotBlockExtension : Extension() {
+    override val name: String = "botblock"
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    override suspend fun setup() {
+        event<ReadyEvent> {
+            startLoop()
+        }
+    }
+
+    private fun startLoop() {
+        scope.launch {
+            delay(Duration.minutes(Config.BOTBLOCK_DELAY))
+            try {
+                kord.postStats(Config.BOT_LIST_TOKENS)
+            } catch (e: KtorRequestException) {
+                LOG.error(e) { "Could not post stats" }
+            }
+        }
+    }
+
+    override suspend fun unload() {
+        scope.cancel()
+    }
+}

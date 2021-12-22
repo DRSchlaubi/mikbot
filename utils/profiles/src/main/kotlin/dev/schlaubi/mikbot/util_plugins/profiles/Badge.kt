413d8569a7cd99bf7de68724b91b2f18da3f7ef5
NyCode <me@nycode.dev>
2021-12-22 04:51:53 +0100

    Add 2 awesome new badges

--- utils/profiles/src/main/kotlin/dev/schlaubi/mikbot/util_plugins/profiles/Badge.kt
+++ utils/profiles/src/main/kotlin/dev/schlaubi/mikbot/util_plugins/profiles/Badge.kt
@@ -5,5 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 enum class Badge(val emoji: String, val displayName: String) {
     VERIFIED_MIKBOT_DEVELOPER("<:verified_bot_dev:913853771900141590>", "profiles.badges.verified_mikbot_dev"),
-    CONTRIBUTOR("<:github:912756097562054666>", "profiles.badges.contributor")
+    CONTRIBUTOR("<:github:912756097562054666>", "profiles.badges.contributor"),
+    CERTIFIED_CUTIE("<a:love_roll:777944174514274344>", "profiles.badges.certified_cutie"),
+    GOOGLE_SIMP("<:google_color:652560081874845697>", "profiles.badges.google_simp")
 }

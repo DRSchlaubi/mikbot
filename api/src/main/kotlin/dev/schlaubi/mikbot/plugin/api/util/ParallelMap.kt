0d0d120baad3a161f32d22c17e8d1af956f59add
Michael Rittmeister <michael@rittmeister.in>
2022-01-03 22:11:45 +0100

    Add more utils

--- api/src/main/kotlin/dev/schlaubi/mikbot/plugin/api/util/ParallelMap.kt
+++ api/src/main/kotlin/dev/schlaubi/mikbot/plugin/api/util/ParallelMap.kt
@@ -5,16 +5,46 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withPermit
 
+/**
+ * Maps every element of this collection using [mapper] in parallel.
+ *
+ * @param maxConcurrentRequests the maximum amount of concurrent coroutines (`null` means unlimited)
+ */
+public suspend fun <T, S> Collection<T>.parallelMap(
+    maxConcurrentRequests: Int? = null,
+    mapper: suspend (T) -> S
+): List<S> = parallelMapIndexed(maxConcurrentRequests) { _, t -> mapper(t) }
+
+/**
+ * Maps every element of this collection using [mapper] in parallel and filters out `null`.
+ *
+ * @param maxConcurrentRequests the maximum amount of concurrent coroutines (`null` means unlimited)
+ */
 public suspend fun <T, S> Collection<T>.parallelMapNotNull(
     maxConcurrentRequests: Int? = null,
     mapper: suspend (T) -> S?
-): List<S> = parallelMapNotNullIndexed(maxConcurrentRequests) { _, t -> mapper(t) }
+): List<S> = parallelMap(maxConcurrentRequests, mapper).filterNotNull()
 
+/**
+ * Maps every element of this collection using [mapper] in parallel and filters out `null`.
+ *
+ * @param maxConcurrentRequests the maximum amount of concurrent coroutines (`null` means unlimited)
+ */
 public suspend fun <T, S> Collection<T>.parallelMapNotNullIndexed(
     maxConcurrentRequests: Int? = null,
     mapper: suspend (index: Int, T) -> S?
+): List<S> = parallelMapIndexed(maxConcurrentRequests, mapper).filterNotNull()
+
+/**
+ * Maps every element of this collection using [mapper] in parallel.
+ *
+ * @param maxConcurrentRequests the maximum amount of concurrent coroutines (`null` means unlimited)
+ */
+public suspend fun <T, S> Collection<T>.parallelMapIndexed(
+    maxConcurrentRequests: Int? = null,
+    mapper: suspend (index: Int, T) -> S
 ): List<S> {
-    val result = MutableList<S?>(size) { null }
+    val result = ArrayList<S>(size)
 
     val semaphore = maxConcurrentRequests?.let { Semaphore(it) }
 
@@ -37,5 +67,5 @@ public suspend fun <T, S> Collection<T>.parallelMapNotNullIndexed(
         }
     }
 
-    return result.filterNotNull()
+    return result
 }

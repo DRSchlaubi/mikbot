6aeebd59a1a7bfff49102a212d9eb01cbcec9567
StckOverflw <stckoverflw@gmail.com>
2022-01-28 20:07:55 +0100

    Add default language

--- utils/role-selector/build.gradle.kts
+++ utils/role-selector/build.gradle.kts
@@ -1,3 +1,6 @@
+import dev.schlaubi.mikbot.gradle.GenerateDefaultTranslationBundleTask
+import java.util.*
+
 plugins {
     `mikbot-plugin`
     `mikbot-module`
@@ -8,5 +11,29 @@ group = "dev.schlaubi.mikbot"
 version = "1.0.5"
 
 mikbotPlugin {
+    provider.set("StckOverflw")
+    license.set("AGPL-3.0 License")
     description.set("Give Roles on a specific Event")
+}
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+
+    val generateDefaultResourceBundle = task<GenerateDefaultTranslationBundleTask>("generateDefaultResourceBundle") {
+        defaultLocale.set(Locale("en", "GB"))
+    }
+
+    assemblePlugin {
+        dependsOn(generateDefaultResourceBundle)
+    }
 }

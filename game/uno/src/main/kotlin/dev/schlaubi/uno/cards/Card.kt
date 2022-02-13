611195485d64c4f52fb5e59ad82b1bbf1dd3f1ce
Michael Rittmeister <michael@rittmeister.in>
2022-02-13 14:31:33 +0100

    Uno code cleanup - Move abstract cards to Card.kt - Remove unnecessary part of SlapCard logic - Add missing documentation - Check card number before trying to remove CardSwitching7 - Don't add first card to played deck twice - Check for card being playable before checking challenge status - Draw cards correctly in extreme mode

--- game/uno/src/main/kotlin/dev/schlaubi/uno/cards/Card.kt
+++ game/uno/src/main/kotlin/dev/schlaubi/uno/cards/Card.kt
@@ -56,6 +56,54 @@ public sealed interface DrawingCard : Card {
      * Checks whether this card can stack with [card] or not.
      */
     public fun canStackWith(card: PlayedCard): Boolean
+
+}
+
+/**
+ * Abstract implementation of [Card] and [PlayedCard] for cards which have a fixed color.
+ */
+public sealed class ColoredCard : PlayedCard {
+    override fun canBePlayedOn(card: PlayedCard): Boolean = card.color == color
+
+    final override fun play(color: UnoColor): PlayedCard = this
+
+    override fun compareTo(other: Card): Int {
+        return if (other is PlayedCard) {
+            color.compareTo(other.color)
+        } else {
+            0
+        }
+    }
+}
+
+/**
+ * Abstract implementation of colored cards with numbers on them.
+ *
+ * @property number the number of this card
+ *
+ * @see ColoredCard
+ * @see SimpleCard
+ */
+public abstract class NumberedCard : ColoredCard() {
+    public abstract val number: Int
+
+    override fun canBePlayedOn(card: PlayedCard): Boolean {
+        if (super.canBePlayedOn(card)) return true // color check
+        return (card as? NumberedCard)?.number == number
+    }
+
+    override fun compareTo(other: Card): Int {
+        val parent = super.compareTo(other)
+        return if (parent == 0) {
+            if (other is SimpleCard) {
+                number.compareTo(other.number)
+            } else {
+                -1
+            }
+        } else {
+            parent
+        }
+    }
 }
 
 /**

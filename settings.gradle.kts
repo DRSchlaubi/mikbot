3879763bca9026e6d827ccfb5cbe2c0857bdb21e
marie <me@nycode.dev>
2022-04-07 18:17:21 +0200

    Add gradle build scans to ci

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,14 @@
+plugins {
+    id("com.gradle.enterprise") version "3.9"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
 rootProject.name = "mikmusic"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")

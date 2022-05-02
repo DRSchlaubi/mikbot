3d599d9c68d8d1d3fecbce36d5aa1b15ab83abc7
Michael Rittmeister <dr@schlau.bi>
2022-05-02 22:41:13 +0000

    [skip ci] gradle: remove dependency locking

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,10 +22,6 @@ allprojects {
         maven("https://nycode.jfrog.io/artifactory/snapshots/")
         maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
-
-    dependencyLocking {
-        lockAllConfigurations()
-    }
 }
 
 subprojects {

a7dcdb010f05ada76575c1ea923f709cbc6dc136
Michael Rittmeister <michael@rittmeister.in>
2022-05-15 02:02:12 +0200

    role-selector: Fix permissions and translations

--- utils/role-selector/build.gradle.kts
+++ utils/role-selector/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.schlaubi.mikbot"
-version = "2.3.0"
+version = "2.4.0"
 
 mikbotPlugin {
     description.set("Give Roles on a specific Event")
--- utils/role-selector/src/main/kotlin/dev/schlaubi/mikbot/utils/roleselector/command/roleselection/RoleMessageCommand.kt
+++ utils/role-selector/src/main/kotlin/dev/schlaubi/mikbot/utils/roleselector/command/roleselection/RoleMessageCommand.kt
@@ -2,10 +2,14 @@ package dev.schlaubi.mikbot.utils.roleselector.command.roleselection
 
 import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
 import dev.schlaubi.mikbot.plugin.api.settings.SettingsModule
+import dev.schlaubi.mikbot.plugin.api.settings.guildAdminOnly
+import dev.schlaubi.mikbot.utils.roleselector.util.setTranslationKey
 
 suspend fun SettingsModule.createRoleMessageCommand() = ephemeralSlashCommand {
     name = "role-selector"
     description = "<never used>"
+    guildAdminOnly()
+    setTranslationKey()
 
     addRoleMessageCommand()
     removeRoleMessageCommand()

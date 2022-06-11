e3ec3a85f33b3d85ff6c2bf63dc899c1a292f27f
Michael Rittmeister <michael@rittmeister.in>
2022-06-11 20:03:54 +0200

    Bump KSP gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     implementation(kotlin("gradle-plugin"))
     implementation("dev.schlaubi", "gradle-plugin", "1.0.0")
-    implementation("com.google.devtools.ksp", "com.google.devtools.ksp.gradle.plugin", "1.6.21-1.0.5")
+    implementation("com.google.devtools.ksp", "com.google.devtools.ksp.gradle.plugin", "1.6.21-1.0.6")
     implementation("org.jlleitschuh.gradle", "ktlint-gradle", "10.3.0")
     implementation(gradleApi())
     implementation(localGroovy())

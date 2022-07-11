6ecdd8cfbd803940d7b52a4aa27e2abeac8a7d41
Marie Ramlow <me@nycode.dev>
2022-07-11 02:53:00 +0200

    dependency updates (#182)
    * chore(deps): update plugin org.jetbrains.kotlin.jvm to v1.7.10
    
    * chore(deps): update plugin org.jetbrains.kotlin.plugin.serialization to v1.7.10
    
    * chore(deps): update dependency dev.inmo:krontab to v0.7.4
    
    * chore(deps): update dependency com.google.apis:google-api-services-youtube to v3-rev20220628-1.32.1
    
    * chore(deps): update dependency com.google.cloud:libraries-bom to v26
    
    * Bump version and fix line endings
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("com.gradle.plugin-publish") version "1.0.0-rc-3"
     `java-gradle-plugin`
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.serialization") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 group = "dev.schlaubi"

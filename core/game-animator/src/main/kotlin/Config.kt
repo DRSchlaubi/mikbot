47d32118b39236b7bff023c0d7172e2d7f087a77
Nicolas Edelmann <niggelgame@gmail.com>
2022-09-07 12:07:13 +0200

    feat: #216

--- core/game-animator/src/main/kotlin/Config.kt
+++ core/game-animator/src/main/kotlin/Config.kt
@@ -4,4 +4,6 @@ import dev.schlaubi.mikbot.plugin.api.EnvironmentConfig
 
 object Config : EnvironmentConfig("") {
     val GAMES by getEnv(emptyList()) { it.split(",").map { Game.parse(it) } }
+
+    val STREAM_URL by getEnv("https://twitch.tv/schlauhibi")
 }

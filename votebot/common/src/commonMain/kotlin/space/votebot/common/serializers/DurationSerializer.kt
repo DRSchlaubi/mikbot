9b8e7403239472db4ad7db76634772584518be74
Michael Rittmeister <michael@rittmeister.in>
2022-09-17 13:41:16 +0200

    Fix VoteBot Duration serializer

--- votebot/common/src/commonMain/kotlin/space/votebot/common/serializers/DurationSerializer.kt
+++ votebot/common/src/commonMain/kotlin/space/votebot/common/serializers/DurationSerializer.kt
@@ -8,11 +8,9 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.nanoseconds
-import kotlin.time.ExperimentalTime
 
-@OptIn(ExperimentalTime::class)
 public object DurationSerializer : KSerializer<Duration> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Duration", PrimitiveKind.LONG)
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("VoteBotDuration", PrimitiveKind.LONG)
 
     override fun serialize(encoder: Encoder, value: Duration): Unit = encoder.encodeLong(value.inWholeNanoseconds)
 

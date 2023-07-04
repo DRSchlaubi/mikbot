ab272e555af1a525090d39a44ab45d23523bf279
Michael Rittmeister <michael@rittmeister.in>
2023-07-04 16:10:23 +0200

    Use ivy repository to install bot instead of downloading it manually

--- gradle-plugin/src/main/kotlin/dev/schlaubi/mikbot/gradle/InstallBotTask.kt
+++ gradle-plugin/src/main/kotlin/dev/schlaubi/mikbot/gradle/InstallBotTask.kt
@@ -3,27 +3,16 @@ package dev.schlaubi.mikbot.gradle
 import org.gradle.api.DefaultTask
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
-import java.net.URI
-import java.net.URL
-import java.nio.channels.Channels
-import java.nio.channels.FileChannel
+import java.io.File
 import java.nio.file.Path
-import java.nio.file.StandardOpenOption
-import kotlin.io.path.createDirectories
-import kotlin.io.path.createFile
-import kotlin.io.path.exists
-
-const val mikbotBinaryUrl = "https://storage.googleapis.com/mikbot-binaries/"
 
 abstract class InstallBotTask : DefaultTask() {
-    @get:Internal
+    @get:OutputDirectory
     internal val testBotFolder: Path
         get() = project.buildDir.resolve("test-bot").resolve(botVersionFromProject()).toPath()
-    private val botArchive: Path
-        get() = testBotFolder.resolve("bot.tar.gz")
 
     @get:Input
     @get:Optional
@@ -34,48 +23,28 @@ abstract class InstallBotTask : DefaultTask() {
         if (botVersion.isPresent && botVersion.get() != botVersionFromProject()) {
             logger.warn("Install task botVersion differs from dependency, please use either the mikbot() dependency or the installTask bot version")
         }
-        if (!project.gradle.startParameter.isRerunTasks && testBotFolder.exists()) {
-            // did work is the most stupid name ever
-            // it actually means whether the task did some work
-            // not whether the task worked
-            didWork = false
-            return
-        }
-        downloadMikbot()
-        extractBot()
+
+        val archive = downloadMikbot()
+        extractBot(archive)
     }
 
-    private fun downloadMikbot() {
-        if (!testBotFolder.exists()) {
-            testBotFolder.createDirectories()
-        }
-        logger.info("Mikbot installation not found! Downloading")
+    private fun downloadMikbot(): File {
+        val runtimeDependency = project.dependencies.create(
+            mapOf(
+                "name" to "mikmusic",
+                "version" to botVersionFromProject(),
+                "ext" to "tar.gz"
+            )
+        )
 
-        val version = botVersionFromProject()
-        val url = "$mikbotBinaryUrl$version/mikmusic-$version.tar.gz"
-        downloadBot(url)
+        return project.configurations.detachedConfiguration(runtimeDependency).resolve().single()
     }
 
-    private fun extractBot() {
+    private fun extractBot(archive: File) {
         project.copy {
-            from(project.tarTree(botArchive.toAbsolutePath()))
+            from(project.tarTree(archive))
             into(testBotFolder)
         }
-
-        project.delete {
-            delete(botArchive.toAbsolutePath())
-        }
-    }
-
-
-    private fun downloadBot(url: String) {
-        if (!botArchive.exists()) {
-            botArchive.createFile()
-        }
-        val website = URI(url).toURL()
-        val readChannel = Channels.newChannel(website.openStream())
-        val writeChannel = FileChannel.open(botArchive, StandardOpenOption.WRITE)
-        writeChannel.transferFrom(readChannel, 0, Long.MAX_VALUE)
     }
 
     internal fun botVersionFromProject(): String =
